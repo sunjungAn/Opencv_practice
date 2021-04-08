c0fc6be0a40ac2929542cae5d96570cf60cf87e0
SunjungAn <55094745+sunjungAn@users.noreply.github.com>
2021-04-08 18:46:30 +0900

    Create webcam_pose_detector(ver.1.2).py

---
+++ webcam_pose_detector(ver.1.2).py
@@ -0,0 +1,57 @@
+from __future__ import division
+import argparse, time, logging, os, math, tqdm, cv2
+from imutils.video import VideoStream
+
+import numpy as np
+import mxnet as mx
+from mxnet import gluon, nd, image
+from mxnet.gluon.data.vision import transforms
+
+import matplotlib.pyplot as plt
+
+import gluoncv as gcv
+from gluoncv import data
+from gluoncv.data import mscoco
+from gluoncv.model_zoo import get_model
+from gluoncv.data.transforms.pose import detector_to_simple_pose, heatmap_to_coord
+from gluoncv.utils.viz import cv_plot_image, cv_plot_keypoints
+
+ctx = mx.cpu()
+detector_name = "ssd_512_mobilenet1.0_coco"
+detector = get_model(detector_name, pretrained=True, ctx=ctx)
+
+detector.reset_class(classes=['person'], reuse_weights={'person':'person'})
+detector.hybridize()
+
+estimators = get_model('simple_pose_resnet18_v1b', pretrained='ccd24037', ctx=ctx)
+estimators.hybridize()
+
+cap = cv2.VideoCapture(0)
+time.sleep(1)
+axes = None
+
+
+while(1):
+    ret, frame = cap.read()
+    frame = mx.nd.array(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).astype('uint8')
+
+    x, frame = gcv.data.transforms.presets.ssd.transform_test(frame, short=512, max_size=350)
+    x = x.as_in_context(ctx)
+    class_IDs, scores, bounding_boxs = detector(x)
+
+    pose_input, upscale_bbox = detector_to_simple_pose(frame, class_IDs, scores, bounding_boxs,
+                                                       output_shape=(128, 96), ctx=ctx)
+    if len(upscale_bbox) > 0:
+        predicted_heatmap = estimators(pose_input)
+        pred_coords, confidence = heatmap_to_coord(predicted_heatmap, upscale_bbox)
+
+        img = cv_plot_keypoints(frame, pred_coords, confidence, class_IDs, bounding_boxs, scores,
+                                box_thresh=0.5, keypoint_thresh=0.2)
+    cv_plot_image(img)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+
+cap.release()
